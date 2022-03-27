1208159607d6b1f688ace817a5bdcb8910ce1759
OleksandrFomenko <fop_fomenko@ukr.net>
2022-03-27 09:37:32 +0300

    loops

---
+++ PYTHON 3.8.6 START/loops.py
@@ -0,0 +1,37 @@
+# counter = 5
+# while counter > 0:
+#     print("Iteration? counter =", counter)
+#     counter -= 1
+# print("_______________________")
+# print(counter)
+# for number in range(5):
+#     print(number)
+# for element in range(4):
+#     if element == 3:
+#         continue
+#     print(element)
+
+# for element in range(4):
+#     if element == 1:
+#         break
+#     print(element)
+# counter = 0
+# endValue = 101
+# while counter < endValue:
+#     if counter % 2 == 0:
+#         print(counter)
+#     counter += 1
+#
+# string_new = ""
+# for letter in "hello":
+#     if letter == "l":
+#         letter = "s"
+#     string_new += letter
+# print(string_new)
+
+back_counter = 99
+end_value = 0
+while back_counter > end_value:
+    if back_counter % 5 == 0:
+        print(back_counter)
+    back_counter -= 1
--- PYTHON 3.8.6 START/variables.py
+++ PYTHON 3.8.6 START/variables.py
@@ -1,17 +1,18 @@
-a = 'hello'
-b = "hello"
-c = '''hello
- world
- and
- hello
- you
- '''
-d = """hello
- world
- and
- hello
- you"""
-print(a)
-print(b)
-print(c)
-print(d)
+if "str":
+    print("True")
+elif "":
+    print("False")
+elif 0j:
+    print("False")
+elif -1:
+    print("True")
+elif 0.0:
+    print("False")
+elif None:
+    print("False")
+elif 1.25:
+    print("True")
+elif False:
+    print("False")
+
+
